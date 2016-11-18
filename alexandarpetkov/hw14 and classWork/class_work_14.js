a7001e1591055fa7f8a3f1f46aea9968270930e1
SashoPetkov <petkovsasho@gmail.com>
2016-11-18 13:14:16 +0200

    homeWorks and self-study

--- alexandarpetkov/hw14 and classWork/class_work_14.js
+++ alexandarpetkov/hw14 and classWork/class_work_14.js
@@ -1,9 +1,10 @@
 
-//var student = {naem: 'Ivan Petrov', grade: 'second', classLetter: 'A', number: '12'};
-// console.log(student);
+var student = {naem: 'Ivan Petrov', grade: 'second', classLetter: 'A', number: '12'};
+//console.log(student);
 
-//var robot = {cordinateX: '', cordinateY: '', moves: '1', orientation: 'north' };
-//
+var robot = {cordinateX: '', cordinateY: '', moves: '1', orientation: 'north' };
+//console.log(robot);
+
 
 
 "use strict";
@@ -25,16 +26,44 @@ var bikes = [
 
 console.log("Need a bike?..");
 
+        // My results home:
+
 function logBikes(bikesList) {
+    var allBikes = '';
 
     for (var i=0; i < bikes.length; i++) {
-        var bike = bikesList[i];
-        console.log(bike.name + ' : ' + bike.price + ' $ ');
+        var bike = (bikesList[i].name + ':' + ' ' + bikesList[i].price + " $");
+        // console.log(bike);
+        allBikes += ("\n" + bike);
     }
+    return allBikes;
 }
 console.log(logBikes(bikes)); // => Drag: 200$
-                            //    RAM: 500$
-                            //    Scott: 1200$
+                              //    RAM: 500$
+                              //    Scott: 1200$
 
 
+var classWorkTask_1 = document.getElementById('task_1');
+classWorkTask_1.innerHTML = 'student is : ' + student;
 
+var classWorkTask_2 = document.getElementById('task_2');
+classWorkTask_2.innerHTML = 'robot is : ' + robot;
+
+var classWorkTask_3 = document.getElementById('task_3');
+classWorkTask_3.innerHTML = logBikes(bikes);
+
+
+            // In Class (Other + My) :
+
+//function logBikes(bikesList) {
+
+//    for (var i=0; i < bikes.length; i++) {
+//        // var bike = bikesList[i];
+//        // console.log(bike.name + ' : ' + bike.price + ' $ ');
+
+//        var bike = (bikesList[i].name + ':' + ' ' + bikesList[i].price + " $");
+//        console.log(bike);
+
+//    }
+//}
+//console.log(logBikes(bikes));
