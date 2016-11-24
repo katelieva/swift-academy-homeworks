3aeda27ba80894ae1f448f603ce36ed7e3219fc9
SashoPetkov <petkovsasho@gmail.com>
2016-11-24 19:09:59 +0200

    commit next

--- alexandarpetkov/self-study_4.js
+++ alexandarpetkov/self-study_4.js
@@ -15,6 +15,10 @@
 //Измислете резултат за случаите на прекалено далечна дата или пък дата в бъдещето.
 //Също така изпишете "Happy birthday!" ако ивмам рожден ден.;
 
+function tellAge(years, months, days) {
+    return  'you are on: ' + years + ' years ' + months + ' months ' + 'and ' + days + ' days';
+}
+
 function howOldAmI(searchAge) {
 
     var myArray = searchAge.split('-');
@@ -25,49 +29,48 @@ function howOldAmI(searchAge) {
 
     var myDate = new Date((myYear), (myMonth+1), (myDay));
     var nowDate = new Date();
-
-
     var standYears = nowDate - myDate;
-
-
-    var years = Math.round(standYears/(1000*60*60*24*7*52));
-    var months = Math.round((nowDate.getMonth()+1) - myMonth);
-    var days = Math.round(nowDate.getDate() - myDay);
+    // var years = Math.round(standYears/(1000*60*60*24*7*52));
+    var years = nowDate.getFullYear() - myYear;
+    var months = (nowDate.getMonth()+1) - myMonth;
+    var days = nowDate.getDate() - myDay;
 
     var myAge;
 
     // HOW to DRY code ????
 
     if (days < 0) {
-        myAge = 'you are on: ' + years + ' years ' + months + ' months ' + 'and ' + days + ' days';
+        myAge = tellAge(years, months, days);
         days = 30 + days;
         years = years - 1;
         months = 12 + months -1;
+
         if (months < 0) {
             months = 12 + Math.round((nowDate.getMonth()+1) - myMonth);
             years = years - 1;
 
-            myAge = 'you are on: ' + years + ' years ' + months + ' months ' + 'and ' + days + ' days';
-            console.log(myAge)
+            myAge = tellAge(years, months, days);
+            // console.log(myAge)
         } else {
 
-            myAge = 'you are on: ' + years + ' years ' + months + ' months ' + 'and ' + days + ' days';
-            console.log(myAge)
+            myAge = tellAge(years, months, days);
+            // console.log(myAge)
         }
     }else if (months < 0) {
         months = 12 + months;
         years = years - 1;
 
-        myAge = 'you are on: ' + years + ' years ' + months + ' months ' + 'and ' + days + ' days';
-        console.log(myAge)
+        myAge = tellAge(years, months, days);
+        // console.log(myAge)
     } else {
-        myAge = 'you are on: ' + years + ' years ' + months + ' months ' + 'and ' + days + ' days';
-        console.log(myAge)
+        myAge = tellAge(years, months, days);
+        // console.log(myAge)
     }
     // BIRTH-DAY
     if (months == 0 && days == 0) {
-        console.log(myAge = 'you are on: ' + years + ' years ' + months + ' months ' + 'and ' + days + ' days' + '   Happy birthday!!!')
+        console.log(myAge = tellAge(years, months, days) + '   Happy birthday!!!')
     }
+        return myAge;
 
 }
 console.log(howOldAmI('12-09-1958'));
@@ -76,6 +79,8 @@ console.log(howOldAmI('17-05-1988'));
 // console.log(howOldAmI('21-11-1984'));
 console.log(howOldAmI('26-11-1986'));
 console.log(howOldAmI('03-05-1986'));
+
+console.log(howOldAmI('23-11-1946'));
 
 
 
